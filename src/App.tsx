054d84f33cbf93731ccc28e79016d3866db48f0a
Maria Fernanda Cenci <mariafcenci@gmail.com>
2024-09-09 22:12:29 -0300

    feat: implemented app component and its style

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ToDo from './components/ToDo'
+import ToDoForm from './components/ToDoForm'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade",
+      category: "Trabalho",
+      isCompleted: false
+    },
+    {
+      id: 2,
+      text: "Estudar SI",
+      category: "Estudo",
+      isCompleted: false
+    },
+    {
+      id: 3,
+      text: "Passear cachorro",
+      category: "Pessoal",
+      isCompleted: false
+    }
+
+  ])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex justify-center mt-4'>
+      <div className='bg-white w-3/4 rounded-lg px-10 pb-10'>
+      <h1 className='text-3xl lg:text-4xl my-5 text-center '>To do list</h1>
+        <div className='flex justify-center flex-col items-center '>
+          
+          <div className='w-full '>
+            {todos.map((todo) => (
+              <ToDo todo={todo} />
+            ))}
+
+          </div>
+          
+        </div>
+        <div className='border-t-2'></div>
+        <ToDoForm />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
+
   )
 }
 
